1a030138408a3b867f527877f28dbbb4489f7763
Alex Togo <helloalextogo@gmail.com>
2021-03-29 09:24:11 -0400

    added missing keys to map items, reorganized CharacterPage JSX and replaced some tables with lists

--- src/components/People.js
+++ src/components/People.js
@@ -8,33 +8,37 @@ const People = ({ people }) => {
   return (
     <>
       <table style={{ width: "50%" }}>
-        <tr>
-          <th>Name</th>
-          <th>Birth Year</th>
-          <th>Height</th>
-          <th>Mass</th>
-        </tr>
-        {people.results.map((person, i) => {
-          return (
-            <tr>
-              <td>
-                <Link
-                  className="nameLink"
-                  to={`/character/${person.url.match(regex)}`}
-                >
-                  {person.name}
-                </Link>
-              </td>
-              <td>{person.birth_year}</td>
-              <td>
-                {person.height === "unknown" ? "unknown" : person.height + "cm"}
-              </td>
-              <td>
-                {person.mass === "unknown" ? "unknown" : person.mass + "kg"}
-              </td>
-            </tr>
-          );
-        })}
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Birth Year</th>
+            <th>Height</th>
+            <th>Mass</th>
+          </tr>
+          {people.results.map((person, i) => {
+            return (
+              <tr key={i}>
+                <td>
+                  <Link
+                    className="nameLink"
+                    to={`/character/${person.url.match(regex)}`}
+                  >
+                    {person.name}
+                  </Link>
+                </td>
+                <td>{person.birth_year}</td>
+                <td>
+                  {person.height === "unknown"
+                    ? "unknown"
+                    : person.height + "cm"}
+                </td>
+                <td>
+                  {person.mass === "unknown" ? "unknown" : person.mass + "kg"}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </>
   );
--- src/pages/CharacterPage.js
+++ src/pages/CharacterPage.js
@@ -40,42 +40,46 @@ const CharacterPage = ({ match }) => {
             <h3>Info:</h3>
             <div className="infoBox">
               <table style={{ width: "100%" }}>
-                <tr>
-                  <th>Height</th>
-                  <th>Mass</th>
-                  <th>Hair Color:</th>
-                  <th>Skin Color</th>
-                  <th>Eye Color</th>
-                  <th>Birth Year</th>
-                  <th>Gender</th>
-                  <th>Species</th>
-                  <th>Homeworld</th>
-                </tr>
-                <tr>
-                  <td>
-                    {person.height === "unknown"
-                      ? "unknown"
-                      : person.height + "cm"}
-                  </td>
-                  <td>
-                    {person.mass === "unknown" ? "unknown" : person.mass + "kg"}
-                  </td>
-                  <td>{person.hair_color}</td>
-                  <td>{person.skin_color}</td>
-                  <td>{person.eye_color}</td>
-                  <td>{person.birth_year}</td>
-                  <td>{person.gender}</td>
-                  {person.species.length === 0 ? (
-                    <td>unknown</td>
-                  ) : (
-                    <ul>
-                      {person.species.map((species, i) => {
-                        return <li key={i}>{species.name}</li>;
-                      })}
-                    </ul>
-                  )}
-                  <td>{person.homeworld.name}</td>
-                </tr>
+                <tbody>
+                  <tr>
+                    <th>Height</th>
+                    <th>Mass</th>
+                    <th>Hair Color:</th>
+                    <th>Skin Color</th>
+                    <th>Eye Color</th>
+                    <th>Birth Year</th>
+                    <th>Gender</th>
+                    <th>Species</th>
+                    <th>Homeworld</th>
+                  </tr>
+                  <tr>
+                    <td>
+                      {person.height === "unknown"
+                        ? "unknown"
+                        : person.height + "cm"}
+                    </td>
+                    <td>
+                      {person.mass === "unknown"
+                        ? "unknown"
+                        : person.mass + "kg"}
+                    </td>
+                    <td>{person.hair_color}</td>
+                    <td>{person.skin_color}</td>
+                    <td>{person.eye_color}</td>
+                    <td>{person.birth_year}</td>
+                    <td>{person.gender}</td>
+                    {person.species.length === 0 ? (
+                      <td>unknown</td>
+                    ) : (
+                      <ul>
+                        {person.species.map((species, i) => {
+                          return <li key={i}>{species.name}</li>;
+                        })}
+                      </ul>
+                    )}
+                    <td>{person.homeworld.name}</td>
+                  </tr>
+                </tbody>
               </table>
             </div>
           </div>
@@ -83,53 +87,49 @@ const CharacterPage = ({ match }) => {
           <div className="boxContainer">
             <h3>Vehicles:</h3>
             <div className="infoBox">
-              <table>
-                <tr>
-                  <th>Starships</th>
-                  <th>Other Vehicles</th>
-                </tr>
-                <tr>
+              <span>
+                <p>
+                  <b>Starships</b>
+                </p>
+                <ul>
                   {person.starships.length === 0 ? (
-                    <td>unknown</td>
+                    <li>unknown</li>
                   ) : (
-                    <ul>
+                    <>
                       {person.starships.map((starship, i) => {
                         return <li key={i}>{starship.name}</li>;
                       })}
-                    </ul>
+                    </>
                   )}
-                  <td>
-                    {person.vehicles.length === 0 ? (
-                      <ul>
-                        <li>unknown</li>
-                      </ul>
-                    ) : (
-                      <ul>
-                        {person.vehicles.map((vehicle, i) => {
-                          return <li key={i}>{vehicle.name}</li>;
-                        })}
-                      </ul>
-                    )}
-                  </td>
-                </tr>
-              </table>
+                </ul>
+                <p>
+                  <b>Other Vehicles</b>
+                </p>
+                <ul>
+                  {person.vehicles.length === 0 ? (
+                    <li>unknown</li>
+                  ) : (
+                    <>
+                      {person.vehicles.map((vehicle, i) => {
+                        return <li key={i}>{vehicle.name}</li>;
+                      })}
+                    </>
+                  )}
+                </ul>
+              </span>
             </div>
 
             <div>
               <h3>Films:</h3>
               <div className="infoBox">
-                <table>
-                  <tr>
-                    <th>Film Name</th>
-                  </tr>
-                  <tr>
-                    <ul>
-                      {person.films.map((film, i) => {
-                        return <li key={i}>{film.title}</li>;
-                      })}
-                    </ul>
-                  </tr>
-                </table>
+                <p>
+                  <b>Film Name</b>
+                </p>
+                <ul>
+                  {person.films.map((film, i) => {
+                    return <li key={i}>{film.title}</li>;
+                  })}
+                </ul>
               </div>
             </div>
           </div>
